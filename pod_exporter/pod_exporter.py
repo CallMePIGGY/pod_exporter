b6437543c1b8a6781cc26906179a22062e388cc6
俊宏 邱 <junhong.qiu@outlook.com>
2019-01-10 18:55:23 +0800

    Add node available memory metrics

--- pod_exporter/pod_exporter.py
+++ pod_exporter/pod_exporter.py
@@ -16,10 +16,11 @@ def getMetrics(url):
     # Convert bytes to string type and string type to dict
     string = response.read().decode('utf-8')
     json_obj = json.loads(string)
-    node = Node('','','')
+    node = Node('','','', '')
     node.name = json_obj['node']['nodeName']
     node.cpu = json_obj['node']['cpu']['usageCoreNanoSeconds'] / 1000000000
-    node.memory = json_obj['node']['memory']['usageBytes']
+    node.availableMemory = json_obj['node']['memory']['availableBytes']
+    node.workingSetMemory = json_obj['node']['memory']['workingSetBytes']
 
     pods_array = json_obj['pods']
 
@@ -30,14 +31,14 @@ def getMetrics(url):
         pod.name = p_item['podRef']['name']
         pod.namespace = p_item['podRef']['namespace']
         pod.cpu = p_item['cpu']['usageCoreNanoSeconds'] / 1000000000
-        pod.memory = p_item['memory']['workingSetBytes']
+        pod.workingSetMemory = p_item['memory']['workingSetBytes']
         containers_array = p_item['containers']
         containers_list = []
         for c_item in containers_array:
             container = Container('','','')
             container.name = c_item['name']
             container.cpu = c_item['cpu']['usageCoreNanoSeconds'] / 1000000000
-            container.memory = c_item['memory']['workingSetBytes']
+            container.workingSetMemory = c_item['memory']['workingSetBytes']
             containers_list.append(container)
         pod.containers = containers_list
         pods_list.append(pod)
@@ -52,28 +53,30 @@ format = "%(asctime)s - %(levelname)s [%(name)s] %(threadName)s %(message)s"
 logging.basicConfig(level=logging.INFO, format=format)
 
 g1 = prom.Gauge('node_cpu_usage_seconds_total', 'CPU useage of the node', labelnames=['node_name'])
-g2 = prom.Gauge('node_memory_usage_bytes', 'Memory useage of the node', labelnames=['node_name'])
-g3 = prom.Gauge('pod_cpu_usage_seconds_total', 'CPU useage of the node', labelnames=['namespace', 'pod_name'])
-g4 = prom.Gauge('pod_memory_usage_bytes', 'Memory useage of the node', labelnames=['namespace', 'pod_name'])
-g5 = prom.Gauge('container_cpu_usage_seconds_total', 'CPU useage of the node', labelnames=['namespace', 'pod_name','container_name'])
-g6 = prom.Gauge('container_memory_usage_bytes', 'Memory useage of the node', labelnames=['namespace', 'pod_name','container_name'])
-g7 = prom.Gauge('kube_pod_container_info', 'Info of the pod', labelnames=['namespace', 'node_name', 'pod_name','container_name'])
+g2 = prom.Gauge('node_memory_available_bytes', 'Memory available of the node', labelnames=['node_name'])
+g3 = prom.Gauge('node_memory_usage_bytes', 'Memory useage of the node', labelnames=['node_name'])
+g4 = prom.Gauge('pod_cpu_usage_seconds_total', 'CPU useage of the node', labelnames=['namespace', 'pod_name'])
+g5 = prom.Gauge('pod_memory_usage_bytes', 'Memory useage of the node', labelnames=['namespace', 'pod_name'])
+g6 = prom.Gauge('container_cpu_usage_seconds_total', 'CPU useage of the node', labelnames=['namespace', 'pod_name','container_name'])
+g7 = prom.Gauge('container_memory_usage_bytes', 'Memory useage of the node', labelnames=['namespace', 'pod_name','container_name'])
+g8 = prom.Gauge('kube_pod_container_info', 'Info of the pod', labelnames=['namespace', 'node_name', 'pod_name','container_name'])
 
 async def compute_rate(url):
     while True:
         stats = getMetrics(url)
         logging.info("nodename: {} value {}".format(stats.node.name, stats.node.cpu))
         g1.labels(node_name=stats.node.name).set(stats.node.cpu)
-        g2.labels(node_name=stats.node.name).set(stats.node.memory)
+        g2.labels(node_name=stats.node.name).set(stats.node.availableMemory)
+        g3.labels(node_name=stats.node.name).set(stats.node.workingSetMemory)
         pods_array = stats.pods
         for p_item in pods_array:
-            g3.labels(namespace=p_item.namespace, pod_name=p_item.name).set(p_item.cpu)
-            g4.labels(namespace=p_item.namespace, pod_name=p_item.name).set(p_item.memory)
+            g4.labels(namespace=p_item.namespace, pod_name=p_item.name).set(p_item.cpu)
+            g5.labels(namespace=p_item.namespace, pod_name=p_item.name).set(p_item.workingSetMemory)
             containers_array = p_item.containers
             for c_item in containers_array:
-                g5.labels(namespace=p_item.namespace, pod_name=p_item.name, container_name=c_item.name).set(c_item.cpu)
-                g6.labels(namespace=p_item.namespace, pod_name=p_item.name, container_name=c_item.name).set(c_item.memory)
-                g6.labels(namespace=p_item.namespace, node_name=stats.node.name, pod_name=p_item.name, container_name=c_item.name)
+                g6.labels(namespace=p_item.namespace, pod_name=p_item.name, container_name=c_item.name).set(c_item.cpu)
+                g7.labels(namespace=p_item.namespace, pod_name=p_item.name, container_name=c_item.name).set(c_item.workingSetMemory)
+                g8.labels(namespace=p_item.namespace, node_name=stats.node.name, pod_name=p_item.name, container_name=c_item.name)
         await asyncio.sleep(1)
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
--- pod_exporter/stats.py
+++ pod_exporter/stats.py
@@ -1,24 +1,26 @@
 class Node:
-    def __init__(self, name, cpu, memory):
+    def __init__(self, name, cpu, availableMemory, workingSetMemory):
         self.name = name
         self.cpu = cpu
-        self.memory = memory
+        self.availableMemory = availableMemory
+        self.workingSetMemory = workingSetMemory
 
 class Pod:
-    def __init__(self, name, namespace, cpu, memory, containers):
+    def __init__(self, name, namespace, cpu, workingSetMemory, containers):
         self.name = name
         self.namespace = namespace
         self.cpu = cpu
-        self.memory = memory
+        self.workingSetMemory = workingSetMemory
         self.containers = containers
 
 class Container:
-    def __init__(self, name, cpu, memory):
+    def __init__(self, name, cpu, workingSetMemory):
         self.name = name
         self.cpu =  cpu
-        self.memory = memory
+        self.workingSetMemory = workingSetMemory
 
 class Stats:
     def __init__(self, node, pods):
         self.node = node
         self.pods = pods
+        
